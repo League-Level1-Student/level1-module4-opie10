e30a58bf58ec2bb17f95190aa85caec0b3804622
augie <augie@keatinge.com>
2020-07-13 18:32:19 -0700

    ummm

--- src/_12_slot_machine/eleven.java
+++ src/_12_slot_machine/eleven.java
@@ -1,5 +1,7 @@
 package _12_slot_machine;
 
 public class eleven {
-
+public static void main(String[] args) {
+	new seven().run();
+}
 }
--- src/_12_slot_machine/seven.java
+++ src/_12_slot_machine/seven.java
@@ -1,5 +1,12 @@
 package _12_slot_machine;
 
 public class seven {
-
+public void run() {
+	
+	
+	
+	
+	
+	
+}
 }
